399b37fb7b15be938aa9223f4a1ccee28782cdd1
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-10-17 17:07:37 +0300

    semi done

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,100 @@
+"use client";
+import { Button } from "bootstrap";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [counter, setCounter] = useState(0);
+  const [bank, setBank] = useState(0);
+  const [isButtonVisibility, setIsButtonVisibility] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const viewVisability = () => {
+    setIsButtonVisibility(true);
+  };
+  const removeVisability = () => {
+    setIsButtonVisibility(false);
+  };
+
+  const toggleVisibilty = () => {
+    setIsButtonVisibility(!isButtonVisibility);
+  };
+  // const bankAccount = (addToBank) => {
+  //   if (addToBank > 10) {
+  //     viewVisability();
+  //     return addToBank + 10;
+  //   }
+  //   return addToBank;
+  // };
+
+  const powerUp = (power, modifer) => {
+    return power + 2 * modifer;
+  };
+
+  const firstPowerup = (powerup) => {
+    return powerup + 2; // 2 * 1
+  };
+  const secondPowerup = (powerup) => {
+    return powerup + 10; // 2 * 5
+  };
+  const thirdPowerup = (powerup) => {
+    return powerup + 50; // 2 * 25
+  };
+  const fourthPowerup = (powerup) => {
+    return powerup + 100; // 2 * 50
+  };
+  const fifthPowerup = (powerup) => {
+    return powerup + 200; // 2 * 100
+  };
+  const sixthPowerup = (powerup) => {
+    return powerup + 500; // 2 * 250
+  };
+  // const seventhPowerup = (powerup) => {
+  //   return powerup + 1000;
+  // };
+
+  const addToCounter = () => {
+    powerUpbutton(bank);
+  };
+
+  const powerUpbutton = (bank) => {
+    if (bank <= 10) {
+      setBank(bank + 1);
+    } else if (bank >= 10 && bank < 20) {
+      if (isButtonVisibility == true) {
+        setBank(firstPowerup(bank));
+        removeVisability();
+      }
+    } else if (bank >= 20 && bank < 100) {
+      setBank(secondPowerup(bank)); // setBank(powerUp(bank, 5))
+    } else if (bank >= 100 && bank < 1000) {
+      setBank(thirdPowerup(bank)); // setBank(powerUp(bank, 25))
+    } else if (bank >= 1000 && bank < 2000) {
+      setBank(fourthPowerup(bank)); // setBank(powerUp(bank, 50))
+    } else if (bank >= 2000 && bank < 5000) {
+      setBank(fifthPowerup(bank)); // setBank(powerUp(bank, 100))
+    } else if (bank >= 5000) {
+      setBank(sixthPowerup(bank));
+    }
+
+    // if(bank >= 5000){
+
+    // } else if(bank >= 2000) {
+
+    // } else if (bank >= 1000) {
+
+    // }
+  };
+
+  return (
+    <div className="bg-blue flex flex-col justify-around items-center w-screen h-screen ">
+      {/* <h1 className="text-6xl	">count:{counter} </h1> */}
+      <h1 className="text-6xl	">count:{bank} </h1>
+      {isButtonVisibility ? (
+        <button className="bg-red-700 text-6xl">hello</button>
+      ) : null}
+      <button className="bg-red-700 text-6xl" onClick={addToCounter}>
+        Add +1
+      </button>
     </div>
   );
 }
